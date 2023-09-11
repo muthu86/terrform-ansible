d5cd8e4705a39e4a8c2336b7b69ac0811a7ede4e
muthu86 <muthu86.eee@gmail.com>
2023-09-11 22:45:16 +0800

    Update user_data_lin.sh

--- user_data_lin.sh
+++ user_data_lin.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo ${mas_public_ip}
-echo ${lin_public_ip}
-sudo yum update -y
-sudo amazon-linux-extras install ansible2 -y
-ansible --version
-ansible localhost -m ping
+useradd vagrant
+echo password | passwd ansible-user --stdin
